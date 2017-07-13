37882e6815811e1833e8a4f1e45440277011c8a6
Ritta Narita <narittan@gmail.com>
2017-07-13 14:39:55 +0900

    Add test

---
+++ test/mruby-fast-strptime.rb
@@ -0,0 +1,63 @@
+
+class ExecTest < MTest::Unit::TestCase
+  def test_year
+    time = Strptime.new('%Y').exec('2015')
+    assert_equal(time.year, 2015)
+  end
+
+  def test_year_to_mon
+    time = Strptime.new('%Y-%m').exec('2015-12')
+    assert_equal(time.year, 2015)
+    assert_equal(time.mon, 12)
+  end
+
+  def test_year_to_day
+    time = Strptime.new('%Y-%m-%d').exec('2015-12-25')
+    assert_equal(time.year, 2015)
+    assert_equal(time.mon, 12)
+    assert_equal(time.day, 25)
+  end
+
+  def test_year_to_hour
+    time = Strptime.new('%Y-%m-%dT%H').exec('2015-12-25T12').utc
+    assert_equal(time.year, 2015)
+    assert_equal(time.mon, 12)
+    assert_equal(time.day, 25)
+    assert_equal(time.hour, 12)
+  end
+
+  def test_year_to_min
+    time = Strptime.new('%Y-%m-%dT%H:%M').exec('2015-12-25T12:34').utc
+    assert_equal(time.year, 2015)
+    assert_equal(time.mon, 12)
+    assert_equal(time.day, 25)
+    assert_equal(time.hour, 12)
+    assert_equal(time.min, 34)
+  end
+
+  def test_year_to_sec
+    time = Strptime.new('%Y-%m-%dT%H:%M:%S').exec('2015-12-25T12:34:56').utc
+    assert_equal(time.year, 2015)
+    assert_equal(time.mon, 12)
+    assert_equal(time.day, 25)
+    assert_equal(time.hour, 12)
+    assert_equal(time.min, 34)
+    assert_equal(time.sec, 56)
+  end
+end
+
+class ExeciTest < MTest::Unit::TestCase
+  def test_to_i
+    time_i = Strptime.new('%Y-%m-%dT%H:%M:%S').execi('2015-12-25T12:34:56')
+    assert_equal(time_i, 1451046896)
+  end
+
+  def test_to_i_with_timezone
+    time_i = Strptime.new('%Y-%m-%dT%H:%M:%S%z').execi('2015-12-25T12:34:56+09:00')
+    assert_equal(time_i, 1451014496)
+  end
+end
+
+if __FILE__ == $0
+  MTest::Unit.new.run
+end
