00173fb9d7d1aea8d7a7bc17c9c0ff474d82a8c2
Ritta Narita <narittan@gmail.com>
2017-11-14 15:02:09 +0900

    Add more tests for parser

--- test/mruby-fast-strptime.rb
+++ test/mruby-fast-strptime.rb
@@ -1,3 +1,153 @@
+class ObjectTest < MTest::Unit::TestCase
+  def test_raise_no_args
+    assert_raise(ArgumentError) do
+      Strptime.new
+    end
+  end
+
+  def test_instance_class
+    pr = Strptime.new("%Y")
+    assert_equal(pr.class, Strptime)
+  end
+
+  def test_parse_Y
+    pr = Strptime.new("%Y")
+    assert_equal(pr.exec("2015").year, 2015)
+    assert_equal(pr.exec("2025").year, 2025)
+  end
+
+  def test_parse_y
+    pr = Strptime.new("%y")
+    assert_equal(pr.exec("15").year, 2015)
+    assert_equal(pr.exec("25").year, 2025)
+    assert_equal(pr.exec("70").year, 1970)
+    assert_equal(pr.exec("68").year, 2068)
+    assert_equal(pr.exec("69").year, 1969)
+  end
+
+  def test_parse_m
+    pr = Strptime.new("%m")
+    assert_equal(pr.exec("12").mon, 12)
+    assert_equal(pr.exec("3").mon, 3)
+  end
+
+  def test_parse_d
+    assert_equal(Strptime.new("%d").exec("28").utc.mday, 28)
+    assert_equal(Strptime.new(" %d").exec(" 28").mday, 28)
+  end
+
+  def test_parse_B
+    pr = Strptime.new("%B")
+    h = pr.exec("May")
+    assert_equal(h.mon, 5)
+    h = pr.exec("January")
+    assert_equal(h.mon, 1)
+  end
+
+  def test_parse_H
+    pr = Strptime.new("%H")
+    h = pr.exec("01").utc
+    assert_equal(h.hour, 1)
+    h = pr.exec("9").utc
+    assert_equal(h.hour, 9)
+    h = pr.exec("23").utc
+    assert_equal(h.hour, 23)
+  end
+
+  def test_parse_M
+    pr = Strptime.new("%M")
+    h = pr.exec("00")
+    assert_equal(h.min, 0)
+    h = pr.exec("59")
+    assert_equal(h.min, 59)
+  end
+
+  def test_parse_d_and_others
+    pr = Strptime.new("%d")
+    assert_equal(pr.exec("10").utc.year, Time.now.year)
+    assert_equal(pr.exec("10").utc.month, Time.now.month)
+    assert_equal(pr.exec("10").day, 10)
+    assert_equal(pr.exec("10").utc.hour, 0)
+    assert_equal(pr.exec("10").min, 0)
+    assert_equal(pr.exec("10").sec, 0)
+  end
+
+  def test_parse_S
+    pr = Strptime.new("%S")
+    h = pr.exec("31")
+    assert_equal(h.sec, 31)
+    pr = Strptime.new("%S")
+    h = pr.exec("59")
+    assert_equal(h.sec, 59)
+    h = pr.exec("60")
+    assert_equal(h.sec, 0)
+  end
+
+  def test_parse_Y_to_z
+    pr = Strptime.new("%Y%m%d%H%M%S%z")
+    assert_equal(pr.exec("20150610102415+0").to_i, 1433931855)
+    assert_equal(pr.exec("20150610102415+9").to_i, 1433931855-9*3600)
+    assert_equal(pr.exec("20150610102415+09").to_i, 1433931855-9*3600)
+    assert_equal(pr.exec("20150610102415+09:00").to_i, 1433931855-9*3600)
+    assert_equal(pr.exec("20150610102415+09:0").to_i, 1433931855-9*3600)
+    assert_equal(pr.exec("20150610102415+0900").to_i, 1433931855-9*3600)
+    assert_equal(pr.exec("20150610102415+090").to_i, 1433931855-9*3600)
+    assert_equal(pr.exec("20150610102415-0102").to_i, 1433931855+1*3600+2*60)
+    assert_equal(pr.exec("20150610102415-1200").to_i, 1433931855+12*3600)
+    assert_equal(pr.exec("20150610102415+1400").to_i, 1433931855-14*3600)
+    assert_equal(pr.exec("20150610102415-1200").to_i, 1433931855+12*3600)
+  end
+
+  def test_parse_Y_to_z_with_symbol
+    pr = Strptime.new("%Y-%m-%d %H:%M:%S")
+    assert_equal(pr.exec("2015-06-11 10:24:15").year, 2015)
+    assert_equal(pr.exec("2015-06-11 10:24:15").month, 6)
+    assert_equal(pr.exec("2015-06-11 10:24:15").day, 11)
+    assert_equal(pr.exec("2015-06-11 10:24:15").utc.hour, 10)
+    assert_equal(pr.exec("2015-06-11 10:24:15").min, 24)
+    assert_equal(pr.exec("2015-06-11 10:24:15").sec, 15)
+  end
+
+  def test_parse_S_and_z
+    pr = Strptime.new("%S%z")
+    assert_equal(pr.exec("12-03:00").year, pr.exec("12-03:00").year)
+    assert_equal(pr.exec("12-03:00").month, Time.now.localtime("-03:00").month)
+    assert_equal(pr.exec("12-03:00").day, Time.now.localtime("-03:00").day)
+    assert_equal(pr.exec("12-03:00").hour, Time.now.localtime("-03:00").hour)
+    assert_equal(pr.exec("12-03:00").min, Time.now.localtime("-03:00").min)
+    assert_equal(pr.exec("12-03:00").sec, 12)
+
+    assert_equal(pr.exec("12+09:00").year, Time.now.localtime("+09:00").year)
+    assert_equal(pr.exec("12+09:00").month, Time.now.localtime("+09:00").month)
+    assert_equal(pr.exec("12+09:00").day, Time.now.localtime("+09:00").day)
+    assert_equal(pr.exec("12+09:00").hour, Time.now.localtime("+09:00").hour)
+    assert_equal(pr.exec("12+09:00").min, Time.now.localtime("+09:00").min)
+    assert_equal(pr.exec("12+09:00").sec, 12)
+
+    assert_equal(pr.exec("12+11:00").year, Time.now.localtime("+11:00").year)
+    assert_equal(pr.exec("12+11:00").month, Time.now.localtime("+11:00").month)
+    assert_equal(pr.exec("12+11:00").day, Time.now.localtime("+11:00").day)
+    assert_equal(pr.exec("12+11:00").hour, Time.now.localtime("+11:00").hour)
+    assert_equal(pr.exec("12+11:00").min, Time.now.localtime("+11:00").min)
+    assert_equal(pr.exec("12+11:00").sec, 12)
+  end
+
+  def test_parse_Ymdz
+    pr = Strptime.new('%Y%m%d %z')
+    assert_equal(pr.exec('20010203 -0200').year, 2001)
+    assert_equal(pr.exec('20010203 -0200').mon, 2)
+    assert_equal(pr.exec('20010203 -0200').day, 3)
+    assert_equal(pr.exec('20010203 -0200').utc.hour, 2)
+    assert_equal(pr.exec('20010203 -0200').min, 0)
+    assert_equal(pr.exec('20010203 -0200').sec, 0)
+  end
+
+  def test_raise_wrong_args
+    assert_raise(RuntimeError) do
+      Strptime.new('%A')
+    end
+  end
+end
 
 class ExecTest < MTest::Unit::TestCase
   def test_year
